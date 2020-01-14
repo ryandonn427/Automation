12164427a866f16473a4e09ee37ba0a82791d5b2
Ryan <ryandonn427@gmail.com>
2020-01-14 16:48:22 -0500

    gps_conversion is a custom file, added this to chapter 4

--- Chapter 4/image_reading.py
+++ Chapter 4/image_reading.py
@@ -4,3 +4,7 @@ import xmltodict
 from gps_conversion import exif_to_decimal,rdf_to_decimal
 
 image1 = Image.open('./images/photo-dublin-a1.jpg')
+print(image1.height)
+print(image1.width)
+print(image1.format)
+
